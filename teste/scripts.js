a2bbf6b8a828be9321612ae8ddebde211143a3ea
Bruno Santos <wanderley.bruno@gmail.com>
2024-02-28 16:11:53 -0300

    att

--- teste/scripts.js
+++
@@ -1,7 +0,0 @@
-function mostrarDetalhes(projeto) {
-    alert("Detalhes do Projeto: " + projeto);
-}
-
-function mostrarMensagem() {
-    alert("Entre em contato pelo email: seuemail@example.com");
-}
