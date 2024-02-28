3cbab43a3e6f3bd48f518cc8b86821391899cf76
Bruno Santos <wanderley.bruno@gmail.com>
2024-02-28 16:10:31 -0300

    adicionando aba teste

---
+++ teste/scripts.js
@@ -0,0 +1,7 @@
+function mostrarDetalhes(projeto) {
+    alert("Detalhes do Projeto: " + projeto);
+}
+
+function mostrarMensagem() {
+    alert("Entre em contato pelo email: seuemail@example.com");
+}
