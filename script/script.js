78b3736858f868c7e5decf225f2421b81911081f
Bruno Santos <wanderley.bruno@gmail.com>
2024-04-15 16:11:24 -0300

    atualizando
    alteração do button ao clicar no menu

--- script/script.js
+++ script/script.js
@@ -1,14 +1,12 @@
-
-function menuDrop(){
+function menuDrop(button){
 
     let menuMob = document.querySelector('.mobile-menu')
     if (menuMob.classList.contains('open')){
         menuMob.classList.remove('open')
-        document.querySelector('.icon').src = "img/barra-de-menu.png"
+        button.querySelector('img').src = "image/barra-de-menu.png"
     } else {
         menuMob.classList.add('open')
-        document.querySelector('.icon').src = "img/excluir.png"
+        button.querySelector('img').src = "image/excluir.png"
     }
-
 
 }
